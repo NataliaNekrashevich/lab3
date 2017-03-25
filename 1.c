3070b5222a3f9be189cef25535e5421ace0e37e3
NataliaNekrashevich <natalianekrashevich@gmail.com>
2017-03-25 08:51:21 +0300

    second commit

--- 1.c
+++ 1.c
@@ -4,7 +4,7 @@ int main()
 {
         printf("Enter your age in years: ");
         int age =0;
-        scanf("%d", &age);      
+        scanf("%d", &age); //enter data     
         age*=365;
         printf("You live about %d days:" , age) ;
         return 0;
