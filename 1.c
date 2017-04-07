eb5e10fab14f15fb5b1b78f20514fbef5275bdb8
Natalia <natalianekrashevich@gmail.com>
2017-04-08 02:08:08 +0300

    Code refactoring

--- 1.c
+++ 1.c
@@ -3,9 +3,9 @@
 int main()
 {
         printf("Enter your age in years: ");
-        int age =0;
+        int age = 0;
         scanf("%d", &age);      
-        age*=365;
-        printf("You live about %d days:" , age) ;
+        age *= 365;
+        printf("You live about %d days:" , age);
         return 0;
 }
